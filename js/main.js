42ab2d648255b6ed73916dfc9d43c8b2ee830c15
Igor Lima <igor_ce@outlook.com>
2017-11-26 18:17:07 -0300

    adicionando menu sanduiche, falta ao clicar em um item no menu sanduiche o menu esconder

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var doc = $('html, body');
-$('.scrollSuave').click(function() {
+
+// fazer um scroll suave
+$('.scroll-suave').click(function() {
 	doc.animate({
 		scrollTop: $($.attr(this, 'href')).offset().top
 	}, 500);
 	return false;
 });
+
+// Abrir side-nav mobile
+$(function(){
+    $(".button-collapse").sideNav();
+    console.log("show");
+});
+
+// Fechar side-nave mobile
+ $(function(){
+     $(".close-nav").sideNav("hide");
+     console.log("hide");
+ });
